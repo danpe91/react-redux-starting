a368020ef4a491e1119cbccd735f2e015b547250
Daniel Pedroza <danpe91@gmail.com>
2018-04-03 21:28:44 -0500

    Section 1, Class 15: Change SearchBar to Class Component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => <input/>
+class SearchBar extends Component {
+	render() {
+		return (
+			<input/>
+		);
+	}
+}
 
 export default SearchBar;
