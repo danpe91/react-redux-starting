cb59a4296f94a28edca5be3ac389738973014681
Daniel Pedroza <danpe91@gmail.com>
2018-04-03 23:15:41 -0500

    Section 2, Class 21-22: Refactor App component to be a class component and call YTSearch API on rendering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
@@ -8,10 +9,26 @@ const API_KEY = 'AIzaSyBMPsZQNdEcDEN_dvKj3gxzcH1AQ1AzdjA';
 // Create a new component. This component should produce
 // some HTML
 
-const App = () => <div>
-			<SearchBar/>
-		</div>
+class App extends Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: 'talent'}, videos => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar/>
+			</div>
+		);
+	}
+}
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
