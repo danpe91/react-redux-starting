ccf36c38c9cca2a53c2c6934e9708ca9566d0fa5
Daniel Pedroza <danpe91@gmail.com>
2018-04-03 21:59:39 -0500

    Section 1, Class 17: Add constructor to SearchBar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
 	// <input onChange={ event => console.log(event.target.value)} /> // Alternative to adding event
 	render() {
 		return (
