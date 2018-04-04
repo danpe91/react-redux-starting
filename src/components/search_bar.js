570bf67922f4fcacaad05c7a041c9aedf02630ac
Daniel Pedroza <danpe91@gmail.com>
2018-04-03 21:46:13 -0500

    Section 1, Class 16: Add event to SearchBar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+	// <input onChange={ event => console.log(event.target.value)} /> // Alternative to adding event
 	render() {
 		return (
-			<input/>
+			<input onChange={this.onInputChange} />
 		);
+	}
+
+	onInputChange(event) {
+		console.log(event.target.value);
 	}
 }
 
